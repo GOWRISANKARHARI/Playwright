852d5198c20853984f7c8a39df1997b8df7bb69b
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-07-07 22:11:14 +0530

    update in  the report

--- generate-report.js
+++ generate-report.js
@@ -1,11 +1,20 @@
+const fs = require('fs');
 const reporter = require('cucumber-html-reporter');
+
+let reportDir = 'playwright-report';
+
+// Ensure directory exists
+if (!fs.existsSync(reportDir)) {
+  fs.mkdirSync(reportDir);
+}
 
 const options = {
   theme: 'bootstrap',
-  jsonFile: 'cucumber_report.json',
-  output: 'playwright-report/cucumber_report.html',
+  jsonFile: `${reportDir}/cucumber_report.json`,
+  output: `${reportDir}/cucumber_report.html`,
   reportSuiteAsScenarios: true,
   launchReport: false,
 };
 
 reporter.generate(options);
+console.log('✅ Cucumber report generated successfully!');
